21332e5e38cbeeefef49339d509b74b8078b562f
Vinit Solanki <2022.vinit.solanki@ves.ac.in>
2025-12-10 15:33:28 +0530

    added the product page in routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 const App = () => {
   return (
     <Router>
@@ -9,6 +10,7 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<div>About</div>} />
         <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
       </Routes>
     </Router>
   )
